adf975450d5ff996403b77b5570149fa531cb91b
Aesha Emad <cxbaha86@gmail.com>
2026-04-16 17:36:45 +0200

    Add Repository to Project

---
+++ FlightBookingSystem/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace FlightBookingSystem.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        Task<List<T>> GetAsync(Expression<Func<T, bool>>? expression = null,
+            bool tracked = false, Expression<Func<T, object>>[]? includes = null);
+
+        Task<T?> GetOneAsync(Expression<Func<T, bool>>? expression = null, bool tracked = false, Expression<Func<T, object>>[]? includes = null);
+
+        Task<int> CommitAsync();
+    }
+
+}
